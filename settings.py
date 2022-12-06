0a1cc030e1d034f69de2cd4486097d9333f6036c
Tarasrivne <105398398+Tarasrivne@users.noreply.github.com>
2022-12-05 21:48:56 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """class to store all game settings"""
+    def __init__(self):
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (220, 230, 240)

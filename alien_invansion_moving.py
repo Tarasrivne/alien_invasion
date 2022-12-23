dbf3f1899140eb4197275f1ad79245923f6b9b14
Tarasrivne <105398398+Tarasrivne@users.noreply.github.com>
2022-12-22 22:41:55 -0500

    Add files via upload

--- alien_invansion_moving.py
+++ alien_invansion_moving.py
@@ -4,6 +4,12 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
 
 class AlienInvansion:
     """class for managing resources and game behavior"""
@@ -32,9 +38,13 @@ class AlienInvansion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.type == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновлює зображення на екрані та відображає новий екран"""

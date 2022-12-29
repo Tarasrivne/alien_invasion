dc636dd53121bcc72fe9826227d657bd151230bc
Tarasrivne <105398398+Tarasrivne@users.noreply.github.com>
2022-12-28 20:42:23 -0500

    Add files via upload
    udated

--- ship.py
+++ ship.py
@@ -6,13 +6,16 @@ class Ship:
     def __init__(self, ai_game):
         """Ініціалізує корабель і задає його початкову позицію"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Завантажує зображення корабля та отримує прямокутник
-        self.image = pygame.image.load('Star_ship2.bmp')
+        self.image = pygame.image.load('ship.image/Star_ship2.bmp')
         self.rect = self.image.get_rect()
         '''Start each new ship at the bottom-center of the screen'''
         self.rect.midbottom = self.screen_rect.midbottom
+        # saving the real coordinate
+        self.x = float(self.rect.x)
         # Флаг переміщення
         self.moving_right = False
         self.moving_left = False

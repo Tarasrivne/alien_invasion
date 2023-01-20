29a9da15820d608016c0366a414ba4c53dcbb5fb
Tarasrivne <105398398+Tarasrivne@users.noreply.github.com>
2023-01-19 21:58:12 -0500

    Add files via upload
    with bullet

--- alian_invasion.py
+++ alian_invasion.py
@@ -1,9 +1,10 @@
-import sys
+ import sys
 
 import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvansion:
     """class for managing resources and game behavior"""
@@ -21,12 +22,14 @@ class AlienInvansion:
 
         pygame.display.set_caption("Alien Invansion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Starting the basic game cycle"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -48,6 +51,8 @@ class AlienInvansion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """reacts to lifting keys"""
@@ -56,10 +61,17 @@ class AlienInvansion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Створення нового снаряду та включення його у групу bullets."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Обновлює зображення на екрані та відображає новий екран"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # display of the last registered screen
         pygame.display.flip()
